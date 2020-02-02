1bb6ea3f7a805c75e717ace6deee6c61037d1034
Wooseop Kim <wooseop.kim.dev@gmail.com>
2020-02-02 20:50:21 +0900

    Remove scripts after rendering

--- js/renderer.js
+++ js/renderer.js
@@ -1,3 +1,6 @@
 const main = document.querySelector('main')
 const render = Handlebars.compile(main.innerHTML)
 main.innerHTML = render(values)
+setTimeout(() => {
+  document.querySelectorAll('script').forEach((x) => x.remove())
+}, 0)
