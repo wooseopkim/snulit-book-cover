a09689cb951aa802f461a02bcf9da8803735de93
Wooseop Kim <wooseop.kim.dev@gmail.com>
2020-07-26 22:29:19 +0900

    Fill in rear page

--- js/values.js
+++ js/values.js
@@ -1,5 +1,11 @@
 const values = {
   title: '청년문학 제20호',
   subtitle: '2020',
-  credits: '서울대학교 총문학연구회',
+  credits: '총문학연구회',
+  'rear_header': '서울대학교 총문학연구' + vertical('회는 회원의 창작물을 모아 회지를 발간합니다.'),
+  'rear_footer': vertical('학기와 방학 중에 합평과 세미나 등 다양한') + ' 활동을 진행하고 있습니다.',
+}
+
+function vertical(text) {
+  return text.split('').join('<br>')
 }
